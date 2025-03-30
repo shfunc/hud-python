1372648b1e7cedb1410056aab3411a102775a90c
lorenss-m <saeclmusic@gmail.com>
2025-03-30 16:55:40 -0700

    0.1.2-alpha release

--- hud/__init__.py
+++ hud/__init__.py
@@ -9,7 +9,7 @@ from hud.environment import Environment, EvalSet, Observation, TaskResult
 from hud.gym import Gym
 from hud.run import Run
 
-__version__ = "0.1.1"
+__version__ = "0.1.2-alpha"
 
 __all__ = [
     "Environment",
