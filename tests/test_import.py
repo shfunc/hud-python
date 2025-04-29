828267e42ea9531aef76c1cb175720f5ef27d90c
lorenss-m <saeclmusic@gmail.com>
2025-04-29 20:12:35 +0800

    linting error, push for release

--- tests/test_import.py
+++ tests/test_import.py
@@ -4,4 +4,4 @@ from __future__ import annotations
 def test_import():
     """Test that the package can be imported."""
     import hud
-    assert hud.__version__ == "0.2.1"
+    assert hud.__version__ == "0.2.2"
