2f7f20b67474b4e94f3ed10f69b15cd19e4ca2cd
Jaideep <jdchawla29@gmail.com>
2025-08-19 22:59:06 -0700

    remove depreciation import

--- hud/utils/__init__.py
+++ hud/utils/__init__.py
@@ -1,10 +1,7 @@
 from __future__ import annotations
 
-from .deprecation import deprecated, emit_deprecation_warning
 from .telemetry import stream
 
 __all__ = [
-    "deprecated",
-    "emit_deprecation_warning",
     "stream",
 ]
